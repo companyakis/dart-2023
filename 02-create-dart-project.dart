377aff7355231c7818cf410a92be61f358364806
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-10-25 15:10:57 +0300

    Create 02-create-dart-project.dart

---
+++ 02-create-dart-project.dart
@@ -0,0 +1,9 @@
+//we can work on a single file as an example "hello.dart"
+
+//but if our project gets bigger, we can create a dart project
+
+//we can manage packages, assets, configuration files...
+
+//open terminal and run:
+
+//dart create project_name
