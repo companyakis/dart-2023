cb1dc23d5bc35bb6df93c01f66ae4a6b39de4b63
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-10-25 15:26:44 +0300

    Create 04-constants.dart

---
+++ 04-constants.dart
@@ -0,0 +1,10 @@
+void main() {
+  //immutable values
+  const year = 1923;
+  const truth = "Peace at home, peace in the world!";
+  const pi = 22 / 7;
+
+  print(year);
+  print(truth);
+  print(pi);
+}
