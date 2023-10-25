77458ab6729434aa64b790e1f460220f3c888279
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-10-25 15:20:55 +0300

    Create 03-variables.dart

---
+++ 03-variables.dart
@@ -0,0 +1,26 @@
+void main() {
+  //storing any value
+  var ad = "mustafa";
+
+  //storing text
+  String country = "Greenland"; //Really?
+
+  //storing integer
+  int age = 99;
+
+  //storing floating
+  double percentage = 55.21;
+
+  //storing any type pf number (both int and double)
+  num price = 45;
+
+  //true or false
+  bool amIRight = true;
+
+  print(ad);
+  print(country);
+  print(age);
+  print(percentage);
+  print(price);
+  print(amIRight);
+}
