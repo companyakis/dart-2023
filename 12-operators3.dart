114419243bb2d279d4510145b468bf8957ec46a9
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-10-26 11:22:52 +0300

    Create 12-operators3.dart

---
+++ 12-operators3.dart
@@ -0,0 +1,22 @@
+void main() {
+  // >, <, >=, <=, ==, !=
+
+  var a = 5;
+  var b = 3;
+
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
+  print(a == b);
+  print(a != b);
+
+  /*
+  true
+  false
+  true
+  false
+  false
+  true
+  */
+}
