0ebcf340a6fe924a5ef396bcb6fb2483f5584706
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-10-26 11:00:35 +0300

    Create 10-operators1.dart

---
+++ 10-operators1.dart
@@ -0,0 +1,16 @@
+void main() {
+  var a = 25;
+  var b = 3;
+
+  //+ addition, - substraction, * multiplication, / division
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+
+  // ~/ integer division
+  print(a ~/ b);
+
+  // % modulus
+  print(a % b);
+}
