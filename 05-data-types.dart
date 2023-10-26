281d74947eeea1a8482f6e45c2f01410e8e7b41d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-10-26 07:35:35 +0300

    Create 05-data-types.dart

---
+++ 05-data-types.dart
@@ -0,0 +1,11 @@
+/*
+built-in data types:
+Numbers -> num, int, double
+Strings -> String
+Booleans -> bool (true, false)
+Lists -> List (ordered group of items)
+Maps -> Map (key-value pairs)
+Sets -> Set (unordered, unique values of same types)
+Runes -> runes (Unicode values of String)
+Null -> null 
+*/
