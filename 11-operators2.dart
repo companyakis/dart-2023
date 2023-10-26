d8e3b7f9e677ae6bc82b823d3c987fce51eaec01
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-10-26 11:17:07 +0300

    Create 11-operators2.dart

---
+++ 11-operators2.dart
@@ -0,0 +1,19 @@
+//Example of split()
+void main() {
+  var a = 10;
+  var b = 8;
+  double c = 32;
+
+  //increment
+  a += 5; // a = a + 5
+  print(a);
+
+  a -= 2; // a = a - 2
+  print(a); //13
+
+  b *= 10; //b = b * 10
+  print(b); //80
+
+  c /= 16; // c = c / 16
+  print(c); //2.0
+}
