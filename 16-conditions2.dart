f93fc17caf9c6eb9a645db237c7bd58d95cc7711
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-10-31 21:20:17 +0300

    Create 16-conditions2.dart

---
+++ 16-conditions2.dart
@@ -0,0 +1,11 @@
+void main() {
+  //if else usage
+
+  String myName = "mustafa";
+
+  if (myName.toUpperCase() == "MUSTAFA") {
+    print("No problem!");
+  } else {
+    print("What happened?");
+  }
+}
