bbb1c04072873b5933e8d281cc8b8420c741d401
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-01 21:24:14 +0300

    Create 20-switch.dart

---
+++ 20-switch.dart
@@ -0,0 +1,30 @@
+void main() {
+  var dayNum = 23;
+
+  switch (dayNum) {
+    case 1:
+      print("Monday");
+      break;
+    case 2:
+      print("Tuesday");
+      break;
+    case 3:
+      print("Wednesday");
+      break;
+    case 4:
+      print("Thursday");
+      break;
+    case 5:
+      print("Friday");
+      break;
+    case 6:
+      print("Saturday");
+      break;
+    case 7:
+      print("Sunday");
+      break;
+    default:
+      print("Select one of these numbers: 1, 2, 3, 4, 5, 6, 7!");
+      break;
+  }
+}
